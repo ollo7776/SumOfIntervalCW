f93248a31cc7df93cd31c4f5bf71fdd95ef51ceb
ollo7776@gmail.com <Charlik111!>
2020-05-09 18:57:28 +0200

    The second version

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(SumOf.sumIntervals(new int[][]{{1, 2}, {6, 10}, {11, 15}}));
+/*
+        System.out.println("0: " + SumOf.sumIntervals(null));
+        System.out.println("9: " + SumOf.sumIntervals(new int[][]{{1, 2}, {6, 10}, {11, 15}}));
+        System.out.println("13: " + SumOf.sumIntervals(new int[][]{{2, 5}, {-1, 2}, {-40, -35}, {6, 8}}));
+        System.out.println("1234: " + SumOf.sumIntervals(new int[][]{{-7, 8}, {-2, 10}, {5, 15}, {2000, 3150}, {-5400, -5338}}));
+        System.out.println("158: " + SumOf.sumIntervals(new int[][]{{-101, 24}, {-35, 27}, {27, 53}, {-105, 20}, {-36, 26}}));
+        System.out.println("0: " + SumOf.sumIntervals(new int[][]{}));
+        System.out.println("0: " + SumOf.sumIntervals(new int[][]{{2, 2}, {5, 5}}));
+*/
+        System.out.println("0: " + SumOf.sumIntervals(new int[][]{{1671, 1671}, {1448, 1448}, {2722, 2722}, {-4395, -4395}, {4872, 4872}, {-299, -299}, {7795, 7795}, {3087, 3087}, {-1342, -1342}, {-4350, -4350}, {-4796, -4796}, {3716, 3716}, {7761, 7761}, {5239, 5239}, {4591, 4591}, {-2002, -2002}, {7530, 7530}, {6502, 6502}, {-1462, -1462}, {-2635, -2635}, {6279, 6279}, {8651, 8651}, {-8911, -8911}, {-2978, -2978}, {3128, 3128}}));
+
     }
 }
--- src/SumOf.java
+++ src/SumOf.java
@@ -1,16 +1,34 @@
+import java.util.HashSet;
+
 public class SumOf {
 
     public static int sumIntervals(int[][] intervals) {
-        int sum = 0;
-        int sumOfJ = 0;
-
+        if (intervals == null) {
+            return 0;
+        }
+        int check = 0;
         for (int i = 0; i < intervals.length; i++) {
-            for (int j = 0; j < intervals[i].length; j++) {
-                sum += intervals[i][intervals[i].length-1]-intervals[i][j];
+            for (int j = 0; j < intervals[i].length - 1; j++) {
+                if (intervals[i][j] != intervals[i][intervals[i].length - 1]) {
+                    check += 1;
+                }
             }
         }
-
-
-        return sum;
+        if (check == 0) {
+            return 0;
+        }
+        HashSet<Integer> intervalsList = new HashSet<>();
+        for (int i = 0; i < intervals.length; i++) {
+            for (int j = 0; j < intervals[i].length - 1; j++) {
+                while (intervals[i][j] < intervals[i][intervals[i].length - 1]) {
+                    intervalsList.add(intervals[i][j]);
+                    intervals[i][j] += 1;
+                }
+            }
+        }
+        //   for (Integer item : intervalsList) {
+        // System.out.println("item: " + item);
+        // }
+        return intervalsList.size();
     }
 }
